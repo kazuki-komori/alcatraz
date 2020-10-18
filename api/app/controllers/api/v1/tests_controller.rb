03d711631b0ef9c852b2c0487692f5b70f8a28e4
Yuya Sumie <monamonacan0926@gmail.com>
2020-10-18 19:29:06 +0900

    api を疎通確認用に

---
+++ api/app/controllers/api/v1/tests_controller.rb
@@ -0,0 +1,9 @@
+module Api
+    module V1
+        class TestsController < ApplicationController
+            def index
+                render json: { status: 'SUCCESS', message: 'tests' }
+            end
+        end
+    end
+end
