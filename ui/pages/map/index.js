7fc6f6a318547b459d920965d3cd80e9aa61c5f1
Yuya Sumie <monamonacan0926@gmail.com>
2020-10-31 18:09:36 +0900

    map 画面を修正

--- ui/pages/map/index.js
+++ ui/pages/map/index.js
@@ -1,12 +1,30 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import styles from '../../styles/Map.module.css'
+import Head from 'next/head'
 
 export default function Map() {
   return (
-    <div className={styles.container}>
-      <Link href="/">
-        <a>トップに戻る</a>
-      </Link>
+    <div>
+      <Head>
+        <title>Alcatraz</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <div className={styles.links}>
+          <Link href="/">
+            <a>トップに戻る</a>
+          </Link>
+        </div>
+        <div className={styles.icons}>
+          <Image
+            src="/mitarashi.png"
+            width={50}
+            height={50}
+          />
+        </div>
+      </main>
     </div>
   )
 }
