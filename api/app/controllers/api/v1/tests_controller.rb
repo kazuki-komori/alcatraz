77b81f8a557a645a64ae31b124537515e9931a23
yamagai <kohei.yamamoto919@gmail.com>
2020-11-07 21:59:18 +0900

    tekton triggers test

--- api/app/controllers/api/v1/tests_controller.rb
+++ api/app/controllers/api/v1/tests_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class TestsController < ApplicationController
       def index
-        render json: { status: 'SUCCESS', message: 'tests' }
+        render json: { status: 'SUCCESS', message: 'tekton tests' }
       end
     end
   end
